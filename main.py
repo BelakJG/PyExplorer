ce88e346de953f20783f52226132e2e26742a824
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-03-13 11:00:14 -0400

    Added a button to travel up a directory

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ def open_items(event):
         current_path = path
         load_files(current_path)
 
+def go_up():
+    global current_path
+    current_path = os.path.dirname(current_path)
+    load_files(current_path)
+
 #sets up main window and geometry
 root = tk.Tk()
 root.title("File Explorer")
@@ -28,6 +33,12 @@ current_path = os.getcwd()
 #widget to display files
 file_list = tk.Listbox(root)
 file_list.pack(fill="both", expand=True)
+load_files(current_path)
+
+file_list.bind("<Double-Button-1>", open_items)
+
+up_button = tk.Button(root, text="Up", command=go_up)
+up_button.pack()
 
 #GUI main event loop
 root.mainloop()
