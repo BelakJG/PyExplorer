5db600f5746a4006647ea143881d850018c35153
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-03-13 10:51:31 -0400

    added widget to display files and method to populate list

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import tkinter as tk
 import os
 
+def load_files(path):
+    file_list.delete(0, tk.END)
+    files = os.listdir(path)
+    for file in files:
+        file_list.insert(tk.END, file)
+
 #sets up main window and geometry
 root = tk.Tk()
 root.title("File Explorer")
 root.geometry("800x600")
+
+#tracks current directory
+current_path = os.getcwd()
+
+#widget to display files
+file_list = tk.Listbox(root)
+file_list.pack(fill="both", expand=True)
 
 #GUI main event loop
 root.mainloop()
